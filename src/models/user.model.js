4022eb5ab5b364e8aeebbb043c533744c7c5996e
ManuYuzu <manuaglez@gmail.com>
2021-08-28 18:38:13 +0100

    user model created

---
+++ src/models/user.model.js
@@ -0,0 +1,55 @@
+import { SchemaTypes, model, Schema } from 'mongoose'
+import mongoosePaginate from 'mongose-paginate'
+import bcrypt from 'bcryptjs'
+
+ const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: [true, 'This email is arleady in use']
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      select: false
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user'
+    },
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 24
+    },
+    surname: {
+      type: String,
+      minlength: 2,
+      maxlength: 24
+    },
+    phone: {
+      type: String,
+      minlength: 5,
+      maxlength: 15
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+userSchema.statics.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10)
+  return await bcrypt.hash(password, salt)
+}
+
+userSchema.statics.comparePassword = async (password, inputPassword) => {
+  return await bcrypt.compare(password, inputPassword)
+}
+
+userSchema.plugin(mongoosePaginate)
+
+export default model('User', userSchema)
